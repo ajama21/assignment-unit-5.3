24059e1ae77a7ec0710edb20bb606ec8c44c3625
Abdirahman Jama <mustalid21@gmail.com>
2023-08-09 08:31:02 -0500

    tested first function, it works

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,13 +2,14 @@ console.log('***** Music Collection *****')
 let collection = [];
 function addToCollection(title, artist, yearPublished) {
     let newAlbum = {
-        title: 'Prime Time',
-        artist: 'Deion Sanders',
-        yearPublished: 1994
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
     }
     collection.push(newAlbum);
     return newAlbum;
 }
+
 console.log(collection);
 addToCollection('Green','Hiroshi Yoshimura', 2020);
 addToCollection('Endless', 'Frank Ocean', 2016);
