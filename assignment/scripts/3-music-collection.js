60e58b1e94ce17d74541bb514360d1361c4276ff
Abdirahman Jama <mustalid21@gmail.com>
2023-08-09 06:16:46 -0500

    added 6 new albums to collections

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,6 +2,18 @@ console.log('***** Music Collection *****')
 let collection = [];
 function addToCollection(title, artist, yearPublished) {
     let newAlbum = {
-        
+        title: 'Prime Time',
+        artist: 'Deion Sanders',
+        yearPublished: 1994
     }
+    collection.push(newAlbum);
+    return newAlbum;
 }
+console.log(collection);
+addToCollection('Green','Hiroshi Yoshimura', 2020);
+addToCollection('Endless', 'Frank Ocean', 2016);
+addToCollection('Computerwelt', 'Kraftwerk', 1981);
+addToCollection('Revolver', 'The Beatlers', 1966 );
+addToCollection('Because the Internet', 'Childish Gambino', 2013);
+addToCollection('Master of Puppets', 'Metallica', 1986);
+console.log(collection);
